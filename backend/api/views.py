c02c3633f51fa2cdadee5aeb699c2823f052cb4b
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-07-02 22:54:32 -0400

    Added view in views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all() #List of all objects when creating new user
+    serializer_class = User #Tells view what data to accept when making new user
+    permission_classes = [AllowAny] #Allows anyone to use view to create new user
