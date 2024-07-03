c4ec7983e83ab7d2cbc4fac57b7783ff156ac4c6
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-07-03 02:24:41 -0400

    Added home page form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
     //Calls getNotes function as soon as page is visited
     useEffect(() => {
         getNotes();
-    }, [])
+    }, []);
 
     //Provides Notes written by user
     const getNotes = () => {
@@ -18,7 +18,7 @@ function Home() {
             .then((res) => res.data)
             .then((data) => 
                 {setNotes(data); 
-                console.log(data)
+                console.log(data);
             })
             .catch((err) => alert(err));
             
@@ -30,31 +30,54 @@ function Home() {
             .then((res) => {
                 if (res.status === 204) alert("Note deleted!");
                 else alert("Failed to delete note.");
+                getNotes();
             })
-            .catch((error) => alert(error));
-        getNotes();
+            .catch((err) => alert(err));
     };
 
     const createNote =  (e) => {
         e.preventDefault()
         api
-            .post("/api/notes/", {content, title}).then((res) => {
+            .post("/api/notes/", {content, title})
+            .then((res) => {
                 if (res.status === 201) alert("Note created!");
                 else alert("Failed to create note.");
+                getNotes();
             })
             .catch((error) => alert(error));
-        getNotes();
     };
 
-    return <div>
+    return (
         <div>
-            <h2>Notes</h2>
+            <div>
+                <h2>Notes</h2>
+            </div>
+            <h2>Create a Note</h2>
+            <form onSubmit={createNote}>
+                <label htmlFor="title">Title:</label>
+                <br/>
+                <input 
+                    type="text" 
+                    id="title" 
+                    name="title" 
+                    required 
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
+                <label htmlFor="content">Content:</label>
+                <br/>
+                <textarea 
+                    id="content" 
+                    name="content" 
+                    required 
+                    onChange={(e) => setContent(e.target.value)}
+                    value={content}
+                ></textarea>
+                <br />
+                <input type="submit" value="Submit"></input>
+            </form>
         </div>
-        <h2>Create a Note</h2>
-        <form onSubmit>
-
-        </form>
-    </div>
+    );
 }
 
 export default Home
