1df56ff88e65f268a658029d5ef873761f200251
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-07-02 23:28:03 -0400

    Added note serializer class

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 
 #Serializer takes JSON data and converts it into python code and vice versa
 from rest_framework import serializers
+from .models import Note
 
 #Inherits from serializer class
 class UserSerializer(serializers.ModelSerializer):
@@ -14,3 +15,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
